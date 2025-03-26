87983c17a4ada97155c95971b94fd3dd6d4a378e
John Nolan <90651514+Fediry@users.noreply.github.com>
2025-03-26 16:07:35 -0700

    Create power_recursive.cpp

---
+++ power_recursive.cpp
@@ -0,0 +1,13 @@
+// write a recursive function that computes x^n
+// power(n)
+// n is a non-negative whole number that the user enters as input
+// x is a floating point number that the user enters as input
+// example: given x=2.3 and n=4, 2.3^4; function should return 27.9841
+// print results
+//
+
+#include <iostream>
+
+using namespace std;
+
+namespace power_recursion {}
