910a9aa861ea8f6c3aaa255e88c95783abee1abe
John <john@DESKTOP-M81049B.attlocal.net>
2025-03-28 21:20:33 -0700

    Added spdlog

--- src/power_recursive.cpp
+++ src/power_recursive.cpp
@@ -6,11 +6,65 @@
 // print results
 //
 
+#include "spdlog/spdlog.h"
 #include <iostream>
 
 using namespace std;
 
 namespace power_recursion {
-    
-    
+
+inline bool is_period(char const ch) { return tolower(ch) == '.'; }
+
+bool is_number(char const ch) { return tolower(ch) >= '0' && ch <= '9'; }
+
+bool is_number(string const str) {
+  if (str.size() == 0) {
+    return false;
+  }
+  // track if there is a period in the string (for floats)
+  bool period_found = false;
+  for (size_t i = 0; i < str.size(); i++) {
+    char const ch = str[i];
+    spdlog::debug("ch={}, is_number(ch)={}, is_period(ch)={}", ch,
+                  is_number(ch), is_period(ch));
+    if (i == 0 && is_number(ch) == false) {
+      return false;
+    }
+    if (!period_found && (is_period(ch) && i < str.size() - 1)) {
+      period_found = true;
+      continue;
+    }
+    if (is_number(ch) == false) {
+      return false;
+    }
+  }
+  return true;
+}
+
+float power(float x, int n) {
+  if (n == 0) {
+    return 1;
+  }
+
+  x *= power(x, n - 1);
+  return x;
+}
+
+void start() {
+  string user_input;
+  cout << "*** Power Recursive ***" << endl;
+
+  while (user_input != q) {
+    cout << "Enter a value for x('q' to quit): ";
+    cin >> user_input;
+    spdlog::debug("user_input={}", user_input);
+  }
+}
+} // namespace power_recursion
+
+int main() {
+  spdlog::set_level(spdlog::level::debug);
+  start();
+
+  return 0;
 }
